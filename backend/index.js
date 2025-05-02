fa1d839a11642463025cbc3229c02b478d83fd67
sidd7013 <sinkarsiddhesh06@gmail.com>
2025-05-02 17:01:57 +0530

    user can add data and save in db

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,18 @@ connectToMongo();
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
+//Available Routes
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
+
+
 app.get('/', (req, res) => {
   res.send('Hello Harry!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ backend/routes/auth.js
@@ -0,0 +1,18 @@
+const express =require('express');
+const router =express.Router();
+const User = require('../models/User');
+
+
+
+//create a user using : POST "/api/auth" . Doesn't require Auth
+router.post('/', (req,res)=>{
+    
+        console.log(req.body);
+        const user = User(req.body);
+        user.save();
+        res.send(req.body);
+ 
+    
+})
+
+module.exports = router
